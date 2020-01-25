2206b2f7d516172337bac23eb5ae05522ebe71da
Jakub Bracha <jakub.bracha@gmail.com>
2020-01-25 16:03:41 +0100

    Added Operator Factory

---
+++ kalkulatorZFabryka/Operators.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace calculator {
+    public interface IOperator
+    {
+        decimal CalculateOperator(decimal a, decimal b);
+        int Weight { get; }
+
+    }
+    class Addition : IOperator
+    {
+        int IOperator.Weight => 1;
+
+        decimal IOperator.CalculateOperator(decimal a, decimal b)
+        {
+            return a - b;
+        }
+    }
+    abstract class OperatorFactory
+    {
+        public abstract IOperator GetOperator();
+    }
+
+    class AdditionFactory : OperatorFactory
+    {
+        public override IOperator GetOperator()
+        {
+            return new Addition();
+        }
+    }
+
+}
+
