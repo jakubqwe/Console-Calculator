490016caca4876d4ee3b511d259c4843e1c741c0
Jakub Bracha <jakub.bracha@gmail.com>
2020-01-25 21:30:16 +0100

    Converted operators enum into factory. Added modulo. Added basic unit test. Operators.cs is put into a dll

---
+++ CalculatorUnitTest/BasicMathTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class BasicMathTests
+    {
+        [TestMethod]
+        public void Add4And3Return7()
+        {
+            var addingOperator = OperatorFactory.Create<Addition>();
+            var result = addingOperator.CalculateOperator(3, 4);
+            Assert.AreEqual(7, result);
+        }
+        [TestMethod]
+        public void Subtract100And8Return92()
+        {
+            var subtractingOperator = OperatorFactory.Create<Subtraction>();
+            var result = subtractingOperator.CalculateOperator(100, 8);
+            Assert.AreEqual(92, result);
+        }
+        [TestMethod]
+        public void Multiply8And2Point5Return20()
+        {
+            var multiplyingOperator = OperatorFactory.Create<Multiplication>();
+            var result = multiplyingOperator.CalculateOperator(8, (decimal)2.5);
+            Assert.AreEqual(20, result);
+        }
+        [TestMethod]
+        public void Divide5By2Return2Point5()
+        {
+            var divisionOperator = OperatorFactory.Create<Division>();
+            var result = divisionOperator.CalculateOperator(5, 2);
+            Assert.AreEqual((decimal)2.5, result);
+        }
+        [TestMethod]
+        public void Power2And10Return1024()
+        {
+            var powerOperator = OperatorFactory.Create<Power>();
+            var result = powerOperator.CalculateOperator(2, 10);
+            Assert.AreEqual(1024, result);
+        }
+        [TestMethod]
+        public void Mod3And2Return1()
+        {
+            var moduloOperator = OperatorFactory.Create<Modulo>();
+            var result = moduloOperator.CalculateOperator(3, 2);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
--- kalkulatorZFabryka/ParseHelper.cs
+++ kalkulatorZFabryka/ParseHelper.cs
@@ -8,45 +8,17 @@ namespace calculator
 {
     public static class ParseHelper
     {
-        public static Decimal OdczytajLiczbe (this string s, ref int startPos)
+        public static Decimal ReadNumber(this string s, ref int startPos)
         {
             int i;
 
 
-            for (i = startPos; i!=s.Length &&(char.IsDigit(s[i]) || s[i] == '.') ; i++) ;
+            for (i = startPos; i != s.Length && (char.IsDigit(s[i]) || s[i] == '.'); i++) ;
 
             var liczba = s.Substring(startPos, i - startPos);
-            startPos += i-startPos-1;
+            startPos += i - startPos - 1;
             return decimal.Parse(liczba);
         }
 
-        public static Operations ToOperations(this string s, int pos)
-        {
-            switch (s[pos])
-            {
-                case '+':
-                    return Operations.Addition;
-                case '-':
-                    return Operations.Subtraction;
-                case '*':
-                    return Operations.Multiplication;
-                case '/':
-                case ':':
-                    return Operations.Division;
-                case '^':
-                    return Operations.Power;
-                case '[':
-                case '{':
-                case '(':
-                    return Operations.LeftBracket;
-                case ']':
-                case '}':
-                case ')':
-                    return Operations.RightBracket;
-                default:
-                    return Operations.Unknown;
-            }
-        }
-        
     }
 }
