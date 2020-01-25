e1281f0bac07b6ce3bbc0a52ebf7020d559f2d6d
Jakub Bracha <jakub.bracha@gmail.com>
2020-01-25 19:26:00 +0100

    Finished writing operators to classes

--- kalkulatorZFabryka/Operators.cs
+++ kalkulatorZFabryka/Operators.cs
@@ -3,29 +3,87 @@
 namespace calculator {
     public interface IOperator
     {
-        decimal CalculateOperator(decimal a, decimal b);
-        int Weight { get; }
+        uint Weight { get; }
 
     }
-    class Addition : IOperator
+    public interface IExecutableOperator : IOperator
     {
-        int IOperator.Weight => 1;
+        decimal CalculateOperator(decimal a, decimal b);
 
-        decimal IOperator.CalculateOperator(decimal a, decimal b)
+    }
+
+    class Default : IOperator
+    {
+        uint IOperator.Weight => 0;
+    }
+    class Addition : IExecutableOperator
+    {
+        uint IOperator.Weight => 2;
+
+        public decimal CalculateOperator(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+    class Subtraction : IExecutableOperator
+    {
+        uint IOperator.Weight => 2;
+
+        public decimal CalculateOperator(decimal a, decimal b)
         {
             return a - b;
         }
     }
-    abstract class OperatorFactory
+    class Multiplication : IExecutableOperator
     {
-        public abstract IOperator GetOperator();
+        uint IOperator.Weight => 3;
+
+        public decimal CalculateOperator(decimal a, decimal b)
+        {
+            return a * b;
+        }
+    }
+    class Division : IExecutableOperator
+    {
+        uint IOperator.Weight => 3;
+
+        public decimal CalculateOperator(decimal a, decimal b)
+        {
+            return a * b;
+        }
+    }
+    class Modulo : IExecutableOperator
+    {
+        uint IOperator.Weight => 3;
+
+        public decimal CalculateOperator(decimal a, decimal b)
+        {
+            return a % b;
+        }
+    }
+    class Power : IExecutableOperator
+    {
+        uint IOperator.Weight => 4;
+
+        public decimal CalculateOperator(decimal a, decimal b)
+        {
+            return (decimal)Math.Pow((double)a, (double)b); ;
+        }
     }
 
-    class AdditionFactory : OperatorFactory
+    class LeftBracket : IOperator
     {
-        public override IOperator GetOperator()
+        public uint Weight => 1;
+    }
+    class RightBracket : IOperator
+    {
+        public uint Weight => 5;
+    }
+    public static class OperatorFactory
+    {
+        public static T Create<T>() where T : IOperator, new()
         {
-            return new Addition();
+            return new T();
         }
     }
 
